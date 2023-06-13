e71271c07ca96a2c392b10a5686c86bfcc2a0177
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2023-06-13 16:08:57 -0700

    [+] vggish and pann impl

--- frechet_audio_distance/model_loader.py
+++ frechet_audio_distance/model_loader.py
@@ -28,3 +28,46 @@ class ModelLoader(ABC):
     @abstractmethod
     def __get_embedding(self, audio: np.ndarray):
         pass
+
+class VGGishModel(ModelLoader):
+    """
+    S. Hershey et al., "CNN Architectures for Large-Scale Audio Classification", ICASSP 2017
+    """
+    def __init__(self, use_pca=False, use_activation=False):
+        super().__init__("vggish")
+        self.use_pca = use_pca
+        self.use_activation = use_activation
+
+    def load_model(self):
+        self.model = torch.hub.load('harritaylor/torchvggish', 'vggish')
+        if not self.use_pca:
+            self.model.postprocess = False
+        if not self.use_activation:
+            self.model.embeddings = nn.Sequential(*list(self.model.embeddings.children())[:-1])
+        self.sr = 16000
+        self.model.eval()
+
+    def __get_embedding(self, audio: np.ndarray) -> np.ndarray:
+        return self.model.forward(audio, self.sr)
+
+class PANNModel(ModelLoader):
+    """
+    Kong et al., "PANNs: Large-Scale Pretrained Audio Neural Networks for Audio Pattern Recognition", IEEE/ACM Transactions on Audio, Speech, and Language Processing 28 (2020)
+    """
+    def __init__(self):
+        super().__init__("pann")
+
+    def load_model(self):
+        model_path = os.path.join(torch.hub.get_dir(), "Cnn14_16k_mAP%3D0.438.pth")
+        if not(os.path.exists(model_path)):
+            torch.hub.download_url_to_file('https://zenodo.org/record/3987831/files/Cnn14_16k_mAP%3D0.438.pth', torch.hub.get_dir())
+        self.model = Cnn14_16k(sample_rate=16000, window_size=512, hop_size=160, mel_bins=64, fmin=50, fmax=8000, classes_num=527)
+        checkpoint = torch.load(model_path, map_location=self.device)
+        self.model.load_state_dict(checkpoint['model'])
+        self.sr = 16000
+        self.model.eval()
+
+    def __get_embedding(self, audio: np.ndarray) -> np.ndarray:
+        with torch.no_grad():
+            out = self.model(torch.tensor(audio).float().unsqueeze(0), None)
+            return out['embedding'].data[0]
