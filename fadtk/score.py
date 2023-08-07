9d1f989f0aedb90c1a539dc7804dcb67e2b18933
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2023-08-07 10:49:33 -0700

    [+] Score launcher

---
+++ fadtk/score.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+
+from .fad import FrechetAudioDistance
+from .model_loader import *
+from .fad_batch import cache_embeddings_files
+
+if __name__ == "__main__":
+    """
+    Launcher for running FAD on two directories using a model.
+    """
+    models = [
+        CLAPLaionModel('audio'), CLAPLaionModel('music'),
+        VGGishModel(), 
+        *(MERTModel(layer=v) for v in range(1, 13)),
+        EncodecEmbModel('24k'), EncodecEmbModel('48k'), 
+        DACModel(),
+        CdpamModel('acoustic'), CdpamModel('content'),
+    ]
+    models = {m.name: m for m in models}
+
+    agupa = ArgumentParser()
+    # Two positional arguments: model and two directories
+    agupa.add_argument('model', type=str, choices=list(models.keys()))
+    agupa.add_argument('baseline_dir', type=str)
+    agupa.add_argument('eval_dir', type=str)
+
+    # Add optional arguments
+    agupa.add_argument('-w', '--workers', type=int, default=8)
+    agupa.add_argument('-s', '--sox-path', type=str, default='/usr/bin/sox')
+
+    args = agupa.parse_args()
+    model = models[args.model]
+
+    fad = FrechetAudioDistance(model, audio_load_worker=args.workers, sox_path=args.sox_path, load_model=False)
+    print(fad.score(args.baseline_dir, args.eval_dir))
+    
