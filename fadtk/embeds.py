a7792564a4e5eb4f2ddfe7bf527613f7416f881c
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2023-08-17 19:24:56 -0700

    [O] Rework fadtk.embeds

---
+++ fadtk/embeds.py
@@ -0,0 +1,27 @@
+from argparse import ArgumentParser
+from .model_loader import *
+from .fad_batch import cache_embedding_files
+
+if __name__ == "__main__":
+    """
+    Launcher for caching embeddings of directories using multiple models.
+    """
+    models = {m.name: m for m in get_all_models()}
+
+    agupa = ArgumentParser()
+    
+    # Accept multiple models and directories with distinct prefixes
+    agupa.add_argument('-m', '--models', type=str, choices=list(models.keys()), nargs='+', required=True)
+    agupa.add_argument('-d', '--dirs', type=str, nargs='+', required=True)
+
+    # Add optional arguments
+    agupa.add_argument('-w', '--workers', type=int, default=8)
+    agupa.add_argument('-s', '--sox-path', type=str, default='/usr/bin/sox')
+
+    args = agupa.parse_args()
+
+    for model_name in args.models:
+        model = models[model_name]
+        for d in args.dirs:
+            log.info(f"Caching embeddings for {d} using {model.name}")
+            cache_embedding_files(d, model, workers=args.workers, sox_path=args.sox_path)
